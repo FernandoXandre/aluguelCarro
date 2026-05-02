aa67cbd8ff0123be333539ae2c954250e15a977d
Fernando <jcf3057@gmail.com>
2026-05-01 22:13:28 -0300

    first commit

---
+++ src/main/java/com/projeto_aluguelCarro/aluguelCarro/AluguelCarroApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_aluguelCarro.aluguelCarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelCarroApplication.class, args);
+	}
+
+}
